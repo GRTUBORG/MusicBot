b0358dd38b2fa8de0aa3df1cd25ecc767bea026d
Dennis <blinhic850@mail.ru>
2023-02-22 19:28:13 +0300

    Update SkipratioCmd.java

--- src/main/java/com/jagrosh/jmusicbot/commands/admin/SkipratioCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/admin/SkipratioCmd.java
@@ -29,7 +29,7 @@ public class SkipratioCmd extends AdminCommand
     public SkipratioCmd(Bot bot)
     {
         this.name = "setskip";
-        this.help = "sets a server-specific skip percentage";
+        this.help = "Задает процент пропуска для конкретного сервера.";
         this.arguments = "<0 - 100>";
         this.aliases = bot.getConfig().getAliases(this.name);
     }
@@ -42,16 +42,16 @@ public class SkipratioCmd extends AdminCommand
             int val = Integer.parseInt(event.getArgs().endsWith("%") ? event.getArgs().substring(0,event.getArgs().length()-1) : event.getArgs());
             if( val < 0 || val > 100)
             {
-                event.replyError("The provided value must be between 0 and 100!");
+                event.replyError("Указанное значение должно находиться в диапазоне от 0 до 100!");
                 return;
             }
             Settings s = event.getClient().getSettingsFor(event.getGuild());
             s.setSkipRatio(val / 100.0);
-            event.replySuccess("Skip percentage has been set to `" + val + "%` of listeners on *" + event.getGuild().getName() + "*");
+            event.replySuccess("Процент пропуска был установлен на `" + val + "%` слушателей на *" + event.getGuild().getName() + "*");
         }
         catch(NumberFormatException ex)
         {
-            event.replyError("Please include an integer between 0 and 100 (default is 55). This number is the percentage of listening users that must vote to skip a song.");
+            event.replyError("Пожалуйста, укажите целое число от 0 до 100 (по умолчанию – 55). Это число представляет собой процент слушающих пользователей, которые должны проголосовать за пропуск песни.");
         }
     }
 }
